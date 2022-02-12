90f1e344dbaa085c3c430259fb74bf7d46527034
snowleopardAlexa <alexaslomski@gmail.com>
2022-02-12 16:27:14 -0500

    add angular charts with chart.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ChartsModule } from 'ng2-charts';
+import { NgChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ChartsModule
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
